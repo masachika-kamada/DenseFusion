e806ae97a7e3e432974d43a46ef946aaeed11be9
masachika <kmd@ruri.waseda.jp>
2022-06-27 10:42:37 +0900

    first commit

---
+++ experiments/scripts/train_linemod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+export CUDA_VISIBLE_DEVICES=0
+
+. ~/anaconda3/etc/profile.d/conda.sh
+conda activate dense_fusion
+
+python3 ./tools/train.py --dataset linemod\
+  --dataset_root ./datasets/linemod/Linemod_preprocessed
