e64305f6ecab8564324169f444d1e0e5c9ffaa6d
masachika <kmd@ruri.waseda.jp>
2022-06-30 04:48:00 +0900

    dockerコンテナ用のファイルを追加

---
+++ dockers/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t dense_fusion -f Dockerfile .
---
+++ dockers/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+XSOCK=/tmp/.X11-unix
+
+XAUTH=/root/.Xauthority
+
+SRC_CONTAINER=/root/dense_fusion
+SRC_HOST="$(pwd)"
+
+xhost local:root
+
+docker run \
+    --name dense_fusion \
+    -it --rm \
+    --volume=$XSOCK:$XSOCK:rw \
+    --volume=$XAUTH:$XAUTH:rw \
+    --volume=$SRC_HOST:$SRC_CONTAINER:rw \
+    --env="XAUTHORITY=${XAUTH}" \
+    --env="DISPLAY=${DISPLAY}" \
+    --privileged -v /dev/bus/usb:/dev/bus/usb \
+    --net=host \
+    --runtime=nvidia \
+    dense_fusion
+
+    # --gpus all \
