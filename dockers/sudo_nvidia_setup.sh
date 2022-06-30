ea0101e35ed09150311c405b39ae1a615c962632
masachika <kmd@ruri.waseda.jp>
2022-06-30 22:12:05 +0900

    dockerの再起動を追加

--- dockers/sudo_nvidia_setup.sh
+++ dockers/sudo_nvidia_setup.sh
@@ -3,3 +3,4 @@ curl -s -L https://nvidia.github.io/nvidia-docker/$(. /etc/os-release;echo $ID$V
 curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | sudo apt-key add - && \
 curl -s -L https://nvidia.github.io/nvidia-container-runtime/$(. /etc/os-release;echo $ID$VERSION_ID)/nvidia-container-runtime.list | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-container-runtime
+service docker restart
